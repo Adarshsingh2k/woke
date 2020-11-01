647dc38160286868d92c05daa3c706ac272dd680
Adarsh Singh <adarshsingh2k>
2020-11-01 13:09:06 +0530

    new commit

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
+var rand=Math.floor((Math.random() * 100000) + 1);
 
+function otpone(){
+
+    alert("OTP sent to Phone Number");
+    console.log(rand);
+};
+
+document.getElementById("verify").addEventListener("click",function(){
+    
+    alert("OTP Verification Sucessfull");
+});
